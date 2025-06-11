5fddde1affdcfc5b817fda57f73850b078e2898d
akadanar <irsyadsabil@proton.me>
2025-06-11 20:22:54 +0700

    feat: Random Crop Comment

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"log"
 	"math/rand/v2"
@@ -27,6 +30,11 @@ type FrameLogs struct {
 	Eps      int  `json:"eps"`
 	Season   int  `json:"season"`
 	IsRandom bool `json:"is_random"`
+}
+
+type PostResp struct {
+	ID     string `json:"id"`
+	PostID string `json:"post_id"`
 }
 
 func loadFrameLogs(filename string) (*FrameLogs, error) {
@@ -87,6 +95,162 @@ func addFilePart(writer *multipart.Writer, fieldName, filePath string) error {
 	return err
 }
 
+func CropRandomSquare(inputBuffer []byte) ([]byte, error) {
+	img, format, err := image.Decode(bytes.NewReader(inputBuffer))
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode image: %v", err)
+	}
+
+	bounds := img.Bounds()
+	width := bounds.Dx()
+	height := bounds.Dy()
+
+	maxCropSize := 680
+	if width < height {
+		if width < maxCropSize {
+			maxCropSize = width
+		}
+	} else {
+		if height < maxCropSize {
+			maxCropSize = height
+		}
+	}
+
+	minCropSize := 200
+	if maxCropSize < minCropSize {
+		return nil, fmt.Errorf("image too small to crop: min 200px required")
+	}
+
+	cropSize := rand.IntN(maxCropSize-minCropSize+1) + minCropSize // minCropSize -> maxCropSize
+
+	maxX := width - cropSize
+	maxY := height - cropSize
+
+	startX := 0
+	startY := 0
+	if maxX > 0 {
+		startX = rand.IntN(maxX + 1)
+	}
+	if maxY > 0 {
+		startY = rand.IntN(maxY + 1)
+	}
+
+	rect := image.Rect(startX, startY, startX+cropSize, startY+cropSize)
+	cropped := img.(interface {
+		SubImage(r image.Rectangle) image.Image
+	}).SubImage(rect)
+
+	// Encode to buffer
+	var outBuf bytes.Buffer
+	switch format {
+	case "png":
+		err = png.Encode(&outBuf, cropped)
+	case "jpeg", "jpg":
+		err = jpeg.Encode(&outBuf, cropped, &jpeg.Options{Quality: 90})
+	default:
+		return nil, fmt.Errorf("unsupported image format: %s", format)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode cropped image: %v", err)
+	}
+
+	return outBuf.Bytes(), nil
+}
+
+// UploadPhoto uploads an image from a buffer to Facebook and returns the media ID
+func UploadPhoto(buffer []byte, accessToken string) (string, error) {
+	var b bytes.Buffer
+	writer := multipart.NewWriter(&b)
+
+	// Add file from buffer
+	part, err := writer.CreateFormFile("source", "image.png")
+	if err != nil {
+		return "", err
+	}
+	_, err = part.Write(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	// Add access_token to the form
+	_ = writer.WriteField("access_token", accessToken)
+
+	err = writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	// Upload to Facebook (unpublished photo)
+	url := fmt.Sprintf("https://graph.facebook.com/v23.0/me/photos?published=false")
+	req, err := http.NewRequest("POST", url, &b)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respBody, _ := io.ReadAll(resp.Body)
+	fmt.Println("Upload Photo Response:", string(respBody))
+
+	// Parse media ID from response
+	var respJSON map[string]interface{}
+	err = json.Unmarshal(respBody, &respJSON)
+	if err != nil {
+		return "", err
+	}
+
+	mediaID, ok := respJSON["id"].(string)
+	if !ok {
+		return "", fmt.Errorf("media ID not found in response")
+	}
+	if errInfo, exists := respJSON["error"]; exists {
+		errMap := errInfo.(map[string]interface{})
+		return "", fmt.Errorf("API Error: %v", errMap["message"])
+	}
+
+	return mediaID, nil
+}
+
+// CommentWithPhoto posts a comment with an image to a specific post
+func CommentWithPhoto(mediaID, message, postID, accessToken string) error {
+	url := fmt.Sprintf("https://graph.facebook.com/v23.0/%s/comments", postID)
+
+	data := map[string]string{
+		"access_token":  accessToken,
+		"message":       message,
+		"attachment_id": mediaID,
+	}
+
+	jsonData, _ := json.Marshal(data)
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	respBody, _ := io.ReadAll(resp.Body)
+	if strings.Contains(string(respBody), "error") {
+		panic(string(respBody))
+	}
+	fmt.Println("Comment Response:", string(respBody))
+
+	return nil
+}
+
 func main() {
 	frame, errFrame := loadFrameLogs("framelogs.json")
 	config, errConfig := loadConfig("config.json")
@@ -145,12 +309,42 @@ func main() {
 						log.Fatalln(err)
 					}
 
-					fmt.Println("Status:", res.Status)
-					fmt.Println("Response:", string(respBody))
 					if strings.Contains(string(respBody), "error") {
 						panic(string(respBody))
 					}
-					
+
+					fmt.Println("Status:", res.Status)
+					fmt.Println("Response:", string(respBody))
+
+					// Unmarshal respBody to struct
+					var post PostResp
+					err = json.Unmarshal(respBody, &post)
+					if err != nil {
+						log.Fatalf("Unmarshal JSON failed: %v", err)
+					}
+					// Random crop part
+					// 1. Upload image from buffer
+					originalImageBytes, err := os.ReadFile(imagePath)
+					if err != nil {
+						log.Fatalln("Failed to read original image:", err)
+					}
+
+					croppedImageBytes, err := CropRandomSquare(originalImageBytes)
+					if err != nil {
+						log.Fatalln("Crop error:", err)
+					}
+
+					mediaID, err := UploadPhoto(croppedImageBytes, config.AccessToken)
+					if err != nil {
+						log.Fatalln("Upload error:", err)
+					}
+
+					// 2. Post a comment with the uploaded image
+					err = CommentWithPhoto(mediaID, "Random Crop:", post.PostID, config.AccessToken)
+					if err != nil {
+						log.Fatalln("Comment error:", err)
+					}
+
 					frame.Season = season
 					frame.Eps = eps
 					frame.Frame = f
@@ -229,10 +423,39 @@ func main() {
 
 		fmt.Println("Status:", res.Status)
 		fmt.Println("Response:", string(respBody))
+
 		if strings.Contains(string(respBody), "error") {
 			panic(string(respBody))
 		}
-		
+		// Unmarshal respBody to struct
+		var post PostResp
+		err = json.Unmarshal(respBody, &post)
+		if err != nil {
+			log.Fatalf("Unmarshal JSON failed: %v", err)
+		}
+		//Random Crop Comment Part
+		// 1. Upload image from buffer
+		originalImageBytes, err := os.ReadFile(imagePath)
+		if err != nil {
+			log.Fatalln("Failed to read original image:", err)
+		}
+
+		croppedImageBytes, err := CropRandomSquare(originalImageBytes)
+		if err != nil {
+			log.Fatalln("Crop error:", err)
+		}
+
+		mediaID, err := UploadPhoto(croppedImageBytes, config.AccessToken)
+		if err != nil {
+			log.Fatalln("Upload error:", err)
+		}
+
+		// 2. Post a comment with the uploaded image
+		err = CommentWithPhoto(mediaID, "Random Crop", post.PostID, config.AccessToken)
+		if err != nil {
+			log.Fatalln("Comment error:", err)
+		}
+
 		time.Sleep(10800 * time.Second)
 	}
 
